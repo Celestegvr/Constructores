7ef047a0aa2a8ff1d472d3d98268370e3d8ef7c1
María Celeste Guevara <143549223+Celestegvr@users.noreply.github.com>
2024-04-30 16:28:28 -0600

    Comentarios

---
+++ Automovil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructoresC_
+{
+    //Clase de nombre Program
+    internal class Program
+    {
+        //Main
+        static void Main(string[] args)
+        {
+        // se crean los objetos y se le asigna el valor a la variable constructor.
+        Automovil AutoNisan= new Automovil("Grand I10", "Gris",4);
+        Automovil AutoFerrari = new Automovil("SX 2050", "Rojo",2);
+
+
+            //información sobre dos instancias de la clase
+        Console.WriteLine("EL MODELO DEL VEHICULO-->{0}, Color {1}, Numero de Puertas-->{2}\n", AutoFerrari.modelo, AutoFerrari.color, AutoFerrari.numpuertas);
+        Console.WriteLine("EL MODELO DEL VEHICULO-->{0}, Color {1}, Numero de Puertas-->{2}\n", AutoNisan.modelo, AutoNisan.color, AutoNisan.numpuertas);
+
+            // Se asignan valores a los objetos que intancian la clase
+
+        /*MIREN COMO SI LO HACEMOS ASI AUMENTA LAS LINEAS DE CODIGO
+                Automovil AutoNisan = new Automovil();
+                Automovil AutoFerrari = new Automovil();
+                AutoNisan.modelo = "Grand I10";
+                AutoNisan.color = "Gris";
+                AutoNisan.numpuertas = 4;
+
+                AutoFerrari.modelo = "SX 2050";
+                AutoFerrari.color = "Rojo";
+                AutoFerrari.numpuertas = 2;
+        */
+
+        /*
+               // INSTANCIAMOS LA CLASE. CREAMOS UN OBJETO
+                  Automovil modelvehicu = new Automovil("Hiunday Grand I10","Rojo", 4);
+
+               // SE IMPRIME EL VALOR INICIAL DEL MODELO 
+                  Console.WriteLine("EL MODELO DEL VEHICULO-->{0}, Color {1}, Numero de Puertas-->{2}\n", modelvehicu.modelo,modelvehicu.color,modelvehicu.numpuertas);
+        */
+
+            // REVISAR QUE SE IMPRIME EL VALOR INICIAL 
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructoresC_
+{
+    //Clase Automovil
+    class Automovil
+    {
+        //dando valores a los atributos del automovil
+        //la variable es public la cual se puede ver fuera de la clase
+        public string modelo, color;
+        public int numpuertas;
+
+        // EL CONSTRUCTRO DEBE TENER EL MISMO NOMBRE DE LA CLASE
+
+        /*La variable contructor llamado Automovil tiene con parametros de string y 
+         enteros creado para inicializar los objetos*/
+       public Automovil(string modeloPa, string ColorPa, int numpuertasPa)
+        {
+       /* SE CREAN 3 PARAMETROS, PARA GESTIONAR LOS ARGUMENTOS QUE SE ENVIARAN DESDE
+          EXTERIOR
+        */
+       //se imprimira el mensaje
+        Console.WriteLine(" SE INVOCA AL CONSTRUCTOR\n");
+
+            //mostrara los valores asignados a los parametros
+            modelo = modeloPa;
+            color = ColorPa;
+            numpuertas = numpuertasPa;
+
+            //this.numpuertas = numpuertasPa;
+        }
+
+    }
+}
